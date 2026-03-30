efd82db8d5c8b47208a649bc69cfd33bedc35802
HebaElgohary <hebasamir535@gmail.com>
2026-03-30 04:52:12 +0200

    fix admin form fields

--- src/components/EditUserForm/EditUserForm.tsx
+++ src/components/EditUserForm/EditUserForm.tsx
@@ -9,19 +9,19 @@ import { authOptions } from '@/server/db/auth'
 import { UserRole } from '@prisma/client'
 import FormFields from '../form-fields/FormFields'
 
-export default async function edituserform() {
+export default async function edituserform({slug}:{slug:string}) {
   const session=await getServerSession(authOptions)
   const role=session?.user?.role
   const locale = await getCurrentLocale()
   const dict=await getDictionary(locale)
-const dictionary=role==UserRole.ADMIN?dict.adminForm['data']:dict.profileForm['data']
-  const formFields = useFormFields({slug:`${Routes.PROFILE}`,dictionary:dictionary,translation:locale })
+const dictionary= role==UserRole.ADMIN?dict.adminForm['data']:dict.profileForm['data']
+  const formFields = useFormFields({slug,dictionary,translation:locale })
  console.log(formFields)
   return (
     <div >
       
         <form action="" className='flex !p-11 gap-5 flex-col md:flex-row justify-center '>
-            edituserform
+        
             <div className='w-full md:w-1/3 flex justify-center items-start'>
                 <Image
                         src="/images/add-photo.jpg"
@@ -32,7 +32,7 @@ const dictionary=role==UserRole.ADMIN?dict.adminForm['data']:dict.profileForm['d
                         priority
                 ></Image>
             </div>
-            <div className='w-full md:w-2/3'>
+            <div className='w-full md:w-2/3 flex flex-col gap-2'>
               {formFields.map((field)=><div  key={field.id} >
               <FormFields {...field} validationsError={';'} />
               </div>)}
